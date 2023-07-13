3875ed92d892464d6f74bfef2266b06b8fc4e2bf
aqni <aqnin@outlook.com>
2023-07-13 14:26:33 +0800

    refactor(tsbs_load_iginx): get ip:port from option

--- cmd/tsbs_load_iginx/main.go
+++ cmd/tsbs_load_iginx/main.go
@@ -10,8 +10,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"sync"
-	"time"
 
 	"github.com/blagojts/viper"
 	"github.com/spf13/pflag"
@@ -24,8 +24,11 @@ import (
 
 // Program option vars:
 var (
-	//session *client.Session
-	doAbortOnExist bool
+	doAbortOnExist       bool
+	connectionSocketList []string
+	defaultTruck         = "unknown"
+	defaultTagK          = []string{"fleet", "driver", "model", "device_version"}
+	defaultTagV          = []string{"unknown", "unknown", "unknown", "unknown"}
 )
 
 // Global vars
@@ -43,19 +46,8 @@ var fatal = log.Fatalf
 func init() {
 	target = initializers.GetTarget(constants.FormatIginx)
 	config = load.BenchmarkRunnerConfig{}
-	// Not all the default flags apply to Iginx
-	// config.AddToFlagSet(pflag.CommandLine)
-	pflag.CommandLine.Uint("batch-size", 10, "Number of items to batch together in a single insert")
-	pflag.CommandLine.Uint("workers", 1, "Number of parallel clients inserting")
-	pflag.CommandLine.Int64("limit", 0, "Number of items to insert (0 = all of them).")
-	pflag.CommandLine.Bool("do-load", true, "Whether to write data. Set this flag to false to check input read speed.")
-	pflag.CommandLine.Bool("no-flow-control", false, "Whether to use flow control. Set this flag to false to load all data first.")
-	pflag.CommandLine.Duration("reporting-period", 10*time.Second, "Period to report write stats")
-	pflag.CommandLine.String("file", "/home/humanfy/tmp_data", "File name to read data from")
-	pflag.CommandLine.Int64("seed", 0, "PRNG seed (default: 0, which uses the current timestamp)")
-	pflag.CommandLine.Uint64("channel-capacity", 100000, "Channel capacity")
-	pflag.CommandLine.String("insert-intervals", "", "Time to wait between each insert, default '' => all workers insert ASAP. '1,2' = worker 1 waits 1s between inserts, worker 2 and others wait 2s")
-	pflag.CommandLine.Bool("hash-workers", false, "Whether to consistently hash insert data to the same workers (i.e., the data for a particular host always goes to the same worker)")
+
+	config.AddToFlagSet(pflag.CommandLine)
 	target.TargetSpecificFlags("", pflag.CommandLine)
 	pflag.Parse()
 
@@ -69,10 +61,12 @@ func init() {
 		panic(fmt.Errorf("unable to decode config: %s", err))
 	}
 
-	//session = client.NewSession("127.0.0.1", "6888", "root", "root")
-	//if err := session.Open(); err != nil {
-	//	log.Fatal(err)
-	//}
+	var connectionStrings = viper.GetString("connStr")
+
+	connectionSocketList = strings.Split(connectionStrings, ",")
+	if len(connectionSocketList) == 0 {
+		log.Fatal("missing 'connStr' flag")
+	}
 
 	config.HashWorkers = false
 	loader = load.GetBenchmarkRunner(config)
